422027bc1c09bc512af19c2e63254db7addef258
Saeed-Soukiah <saeedsoukiah@gmail.com>
2025-03-10 00:22:56 +0300

    tic tac toe c++.cpp

--- tic tac toe c++.cpp
+++ tic tac toe c++.cpp
@@ -1,10 +1,6 @@
 /* 
 Author: Saeed Soukiah 
 Date: December 1, 2024 
-Description: This program implements a simple Tic-Tac-Toe game where the player competes against the computer. 
-The game board is displayed after each move, and the player can choose a position to place their marker ('X'). 
-The computer randomly chooses an empty spot for its marker ('O'). 
-The program checks for a winner or a tie after each move and announces the result. 
 */
 
 #include<iostream>
